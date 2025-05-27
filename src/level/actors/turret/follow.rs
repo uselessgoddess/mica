4f6c9726bfd2ec58db1adfb496f470896bf80f0e
uselessgoddess <dodickgod@gmail.com>
2025-05-27 16:20:58 +0300

    improve target following

--- src/level/actors/turret/follow.rs
+++ src/level/actors/turret/follow.rs
@@ -1,7 +1,4 @@
-use {
-  crate::{level::turret::Target, prelude::*},
-  avian2d::math::FRAC_PI_2,
-};
+use crate::{level::turret::Target, prelude::*};
 
 pub fn plugin(app: &mut App) {
   app.add_systems(Update, follow);
@@ -17,33 +14,18 @@ fn follow(
   mut query: Query<(&mut Transform2D, &Target, &FollowTarget)>,
   time: Res<Time>,
 ) {
-  for (transform, &Target { target, .. }, follow) in query.iter_mut() {
+  for (transform, &Target { target, angle, .. }, follow) in query.iter_mut() {
     let Transform2D { translation: turret, rotation, .. } =
       transform.into_inner();
 
-    let direction = *turret - target;
-
-    if direction.length_squared() < f32::EPSILON {
+    if angle.abs() < f32::EPSILON {
       continue;
     }
 
-    let target_angle = direction.to_angle() + FRAC_PI_2;
-    let current_angle = rotation.as_radians();
+    let rotate = follow.speed * time.delta_secs();
+    let rotate =
+      if angle.abs() <= rotate { angle } else { rotate * angle.signum() };
 
-    let mut angle_diff = target_angle - current_angle;
-
-    if angle_diff.abs() < f32::EPSILON {
-      continue;
-    }
-
-    let rotation_step = follow.speed * time.delta_secs();
-
-    let rotation_amount = if angle_diff.abs() <= rotation_step {
-      angle_diff
-    } else {
-      rotation_step * angle_diff.signum()
-    };
-
-    *rotation = Rot2::radians(current_angle + rotation_amount);
+    *rotation *= Rot2::radians(rotate);
   }
 }
